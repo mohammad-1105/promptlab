41e766df881f519ee7849a0eca2b7bb04ab219d2
Mohammad Aman <mohammadamaan1105@gmail.com>
2024-09-02 08:16:19 +0545

    add prompt model

---
+++ models/prompt.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Prompt extends Document {
+  promptTitle: string;
+  prompt: string;
+  authorImageUrl: string;
+  author: string;
+  tag: string;
+}
+
+const promptSchema: Schema<Prompt> = new Schema(
+  {
+    promptTitle: {
+      type: String,
+      required: [true, "prompt title is required."],
+      minlength: [8, "prompt title should be at least 8 characters."],
+      maxlength: [32, "prompt title should be at most 32 characters."],
+    },
+    prompt: {
+      type: String,
+      required: [true, "prompt is required."],
+      minlength: [40, "prompt should be at least 40 characters."],
+      maxlength: [200, "prompt should be at most 200 characters."],
+    },
+    authorImageUrl: String,
+    author: String,
+    tag: {
+      type: String,
+      required: [true, "tag is required to maintain prompt lists"],
+    },
+  },
+  { timestamps: true }
+);
+
+const PromptModel =
+  mongoose.models.Prompt<Prompt> ||
+  mongoose.model<Prompt>("Prompt", promptSchema);
+export default PromptModel;
