d6917d5f19a0d0d5bfd6c2ab809ef85a4b464da0
Mohammad Aman <mohammadamaan1105@gmail.com>
2024-09-03 09:51:58 +0545

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
         },
       ],
     },
+    env: {
+      KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+      KINDE_POST_LOGOUT_REDIRECT_URL:
+        process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+      KINDE_POST_LOGIN_REDIRECT_URL:
+        process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/api/auth/callback`
+    }
   };
   
   export default nextConfig;
